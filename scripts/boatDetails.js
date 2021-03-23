35b2af4c87c01333f7d4705fa23a8aee60b0fcda
davidRamalho <david.m.ramalho@gmail.com>
2021-03-22 20:02:58 -0600

    remove rentals and details injections + fix grid styling

--- scripts/boatDetails.js
+++ scripts/boatDetails.js
@@ -1,161 +1,25 @@
-
-//GENERATES BOAT DETAILS PAGE
-const createBackToOverview = (name) => {
-  const element = $('.back').append(`
-    <div>
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left-circle-fill" viewBox="0 0 16 16">
-        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.5 7.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
-      </svg>
-      <div class="link-back">&nbsp Back to Overview &nbsp</div>
-      <div>&nbsp ${name}</div>
-    </div>
-  `)
-  return element;
-}
-
-const createRentalCard = (rentalData) => {
-  const element = $('.rentalCard').append(`
-    <div class="title">${rentalData.title}</div>
-    <script type="text/javascript">
-        $(document).ready(function () {
-            $('.sub-img').on({
-                mouseover: function () {
-                    $(this).css({
-                        'cursor': 'pointer',
-                        'box-shadow': '#0080c8 4px 4px'
-                    });
-                },
-                mouseout: function () {
-                    $(this).css({
-                        'cursor': 'default',
-                        'box-shadow': 'none'
-                    });
-                },
-                click: function () {
-                    const imageURL = $(this).attr('src');
-                    $('#mainImage').fadeOut(300, function () {
-                        $(this).attr('src', imageURL);
-                    }).fadeIn(300);
-                }
-            });
-        });
-    </script>
-    <div class="img-gallery">
-      <div class="rentalImage">
-        <img id="mainImage" src="${rentalData.imgSource[0]}" alt="rental-item">
-      </div>
-      <div class='image-roll'>
-        <img class="sub-img" src="${rentalData.imgSource[0]}" alt="rental-item">
-        <img class="sub-img" src="${rentalData.imgSource[1]}" alt="rental-item">
-        <img class="sub-img" src="${rentalData.imgSource[2]}" alt="rental-item">
-        <img class="sub-img" src="${rentalData.imgSource[3]}" alt="rental-item">
-        <img class="sub-img" src="${rentalData.imgSource[4]}" alt="rental-item">
-        <img class="sub-img" src="${rentalData.imgSource[5]}" alt="rental-item">
-        <img class="sub-img" src="${rentalData.imgSource[6]}" alt="rental-item">
-      </div>
-    </div>
-    <div class="card-details">
-      <div class="details-row">
-        <div class="detailed-info">
-          <div class="brand-name">${rentalData.title}</div>
-          Do you crave a life that knows no limit? Take your summer to the next level and live it up in our XT23, an all new crossover MasterCraft that welcomes wakeboards, surfboards, skis and whatever else you please! The XT23 is where classic traditional bow meets new progressives style lines. An Ilmor engine delivers the highest level of performance and is ideal for competitive to recreational water sports. Get ready to make some waves while also enjoying Gen 2 Surf System and a substantial 2600 pounds of ballast. Enjoy an industry-leading quality boat with your loved ones with the XT23.
-        </div>
-        <div class="specs-col">
-          <div class="spec">SPECS</div>
-          <div class="spec-table">
-            <div>
-              <div>Seating:</div>
-              <div>Length:</div>
-              <div>Ballast:</div>
-              <div>Tower w/Shade:</div>
-              <div>Tower Speakers:</div>
-              <div>Engine:</div>
-            </div>
-            <div>
-              <div>16</div>
-              <div>23 ft 4in (7.11m)</div>
-              <div>2660lbs</div>
-              <div>Yes</div>
-              <div>Yes</div>
-              <div>Ilmore 5000 MPI VD 1.57</div>
-            </div>
-          </div>
-          <div class="note">Note: Actual boat and specs may vary from pictures.</div>
-        </div>
-      </div>
-      <div class="pricing-row">
-        <div class="perHour">
-          <div class="perHrTitle">PER HOUR</div>
-          <div class="time">
-            <div>
-              <div>8-10AM</div>
-              <div class="perHrPrice">${rentalData.rates.hourly[0]}</div>
-            </div>
-            <div>
-              <div>1 HOUR</div>
-              <div class="perHrPrice">${rentalData.rates.hourly[1]}</div>
-            </div>
-            <div>
-              <div>2 HOURS</div>
-              <div class="perHrPrice">${rentalData.rates.hourly[2]}</div>
-            </div>
-            <div>
-              <div>4 HOURS</div>
-              <div class="perHrPrice">${rentalData.rates.hourly[3]}</div>
-            </div>
-            <div>
-              <div>8 HOURS</div>
-              <div class="perHrPrice">${rentalData.rates.hourly[4]}</div>
-            </div>
-          </div>
-          <form action="/">
-            <label>
-              <input type="submit" value="&nbsp;&nbsp;&nbsp;&nbsp;BOOK HOURLY"></input>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-              </svg>
-            </label>
-          </form>
-        </div>
-        <div class="perDay">
-          <div class="perDay-title">PER DAY</div>
-          <div class="times">
-            <div>3-6 DAYS</div>
-            <div>7+ DAYS</div>
-          </div>
-          <div class="perDayPrices">
-            <div>${rentalData.rates.daily[0]}</div>
-            <div>${rentalData.rates.daily[1]}</div>
-          </div>
-          <form action="/">
-            <label>
-              <input type="submit" value="&nbsp;&nbsp;&nbsp;&nbsp;BOOK DAILY"></input>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-              </svg>
-            </label>
-          </form>
-        </div>
-      </div>
-    </div>
-  `);
-  return element;
-}
-
 $(document).ready(function() {
-  const title = sessionStorage.getItem('title');
-  const items = JSON.parse(sessionStorage.getItem('items'));
-  let rentalInfo;
-  for (const item of items) {
-    if (item.title === title) {
-      rentalInfo = item;    
-    }
-  }
-
-  createBackToOverview(title);
-  createRentalCard(rentalInfo);
   $('.back').on('click', function() {
-    window.location.href = "/repository/templates/boatRentals.html"
+    window.location.href = "../templates/boatRentals.html"
   });
-
+  $('.sub-img').on({
+        mouseover: function () {
+            $(this).css({
+                'cursor': 'pointer',
+                'box-shadow': '#0080c8 4px 4px'
+            });
+        },
+        mouseout: function () {
+            $(this).css({
+                'cursor': 'default',
+                'box-shadow': 'none'
+            });
+        },
+        click: function () {
+            const imageURL = $(this).attr('src');
+            $('#mainImage').fadeOut(300, function () {
+                $(this).attr('src', imageURL);
+            }).fadeIn(300);
+        }
+    });
 });
--- scripts/boatRentals.js
+++ scripts/boatRentals.js
@@ -1,87 +1,16 @@
-//CREATE RENTAL ELEMENTS 
-const createRentalElement = (rentalInfo) => {
-  const element = $('.items').append(`
-    <div class="item ${rentalInfo.type}">
-      <img src=${rentalInfo.imgSource[0]} alt="rental-item">
-      <div class="item-card">
-        <h2 class="item-title">${rentalInfo.title}</h2>
-        <p class="item-details">Do you crave a life that knows no limit? Take your summer to the next level and...<a class="details" href="/repository/templates/boatDetails.html">View Details</a></p>
-        <div class="rate-tables">
-          <div class="item-rates">
-            <div class="rate-type">PER HOUR</div>
-            <div class="rate-rows">
-              <div>8-10AM:</div>
-              <div>${rentalInfo.rates.hourly[0]}</div>
-            </div>
-            <div class="rate-rows">
-              <div>1HR:</div>
-              <div>${rentalInfo.rates.hourly[1]}</div>
-            </div>
-            <div class="rate-rows">
-              <div>2HRs:</div>
-              <div>${rentalInfo.rates.hourly[2]}</div>
-            </div>
-            <div class="rate-rows">
-              <div>4HRs:</div>
-              <div>${rentalInfo.rates.hourly[3]}</div>
-            </div>
-            <div class="rate-rows">
-              <div>8HRs:</div>
-              <div>${rentalInfo.rates.hourly[4]}</div>
-            </div>
-            <form action="/">
-              <label>
-                <input type="submit" value="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BOOK HOURLY"></input>
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                  <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-                </svg>
-              </label>
-            </form>
-          </div>
-          <div class="item-rates">
-            <div class="rate-type">PER DAY</div>
-            <div class="rate-rows">
-              <div>3-6 DAYS:</div>
-              <div>${rentalInfo.rates.daily[0]}</div>
-            </div>
-            <div class="rate-rows">
-              <div>7+ DAYS:</div>
-              <div>${rentalInfo.rates.daily[1]}</div>
-            </div>
-            <form class="daily" action="/">
-            <label>
-              <input type="submit" value="&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BOOK DAILY"></input>
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
-                <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0zM4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H4.5z"/>
-              </svg>
-            </label>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>`);
-  return element;
-}
-
-const generateRentalItems = (rentalDataArray) => {
-  for (const rentalItem of rentalDataArray) {
-    createRentalElement(rentalItem);
-  }
-}
-
 //FAKE RENTAL DATA
 const rentalData = [
   {
     type: 'wakesurf', 
     title: 'MasterCraft X24',
     imgSource: [
-      '/repository/images/boatRentals/MasterCraftX24.jpg',
-      '/repository/images/boatRentals/ChaparralH2019Sport.jpg',
-      '/repository/images/boatRentals/MasterCraftX24.jpg',
-      '/repository/images/boatRentals/MasterCraftX24.jpg',
-      '/repository/images/boatRentals/MasterCraftX24.jpg',
-      '/repository/images/boatRentals/MasterCraftX24.jpg',
-      '/repository/images/boatRentals/MasterCraftX24.jpg'], 
+      '../images/boatRentals/MasterCraftX24.jpg',
+      '../images/boatRentals/ChaparralH2019Sport.jpg',
+      '../images/boatRentals/MasterCraftX24.jpg',
+      '../images/boatRentals/MasterCraftX24.jpg',
+      '../images/boatRentals/MasterCraftX24.jpg',
+      '../images/boatRentals/MasterCraftX24.jpg',
+      '../images/boatRentals/MasterCraftX24.jpg'], 
     rates: {
       hourly: ['$299', '$339', '$579', '$1,039', '$1,419'], 
       daily: ['$1,299', '$1,069']
@@ -90,7 +19,7 @@ const rentalData = [
   {
     type: 'bowrider', 
     title: 'Chaparral H20 19 Sport',
-    imgSource: ['/repository/images/boatRentals/ChaparralH2019Sport.jpg', '/repository/images/boatRentals/ChaparralH2019Sport.jpg', '/repository/images/boatRentals/ChaparralH2019Sport.jpg', '/repository/images/boatRentals/ChaparralH2019Sport.jpg', '/repository/images/boatRentals/ChaparralH2019Sport.jpg', '/repository/images/boatRentals/ChaparralH2019Sport.jpg', '/repository/images/boatRentals/ChaparralH2019Sport.jpg'], 
+    imgSource: ['../images/boatRentals/ChaparralH2019Sport.jpg', '../images/boatRentals/ChaparralH2019Sport.jpg', '../images/boatRentals/ChaparralH2019Sport.jpg', '../images/boatRentals/ChaparralH2019Sport.jpg', '../images/boatRentals/ChaparralH2019Sport.jpg', '../images/boatRentals/ChaparralH2019Sport.jpg', '../images/boatRentals/ChaparralH2019Sport.jpg'], 
     rates: {
       hourly: ['$299', '$339', '$579', '$1,039', '$1,419'], 
       daily: ['$1,299', '$1,069']
@@ -99,7 +28,7 @@ const rentalData = [
   {
     type: 'pontoon', 
     title: 'South Bay 525E Pontoon',
-    imgSource: ['/repository/images/boatRentals/SouthBayPontoon.jpeg', '/repository/images/boatRentals/SouthBayPontoon.jpeg', '/repository/images/boatRentals/SouthBayPontoon.jpeg', '/repository/images/boatRentals/SouthBayPontoon.jpeg', '/repository/images/boatRentals/SouthBayPontoon.jpeg', '/repository/images/boatRentals/SouthBayPontoon.jpeg', '/repository/images/boatRentals/SouthBayPontoon.jpeg'], 
+    imgSource: ['../images/boatRentals/SouthBayPontoon.jpeg', '../images/boatRentals/SouthBayPontoon.jpeg', '../images/boatRentals/SouthBayPontoon.jpeg', '../images/boatRentals/SouthBayPontoon.jpeg', '../images/boatRentals/SouthBayPontoon.jpeg', '../images/boatRentals/SouthBayPontoon.jpeg', '../images/boatRentals/SouthBayPontoon.jpeg'], 
     rates: {
       hourly: ['$299', '$339', '$579', '$1,039', '$1,419'], 
       daily: ['$1,299', '$1,069']
@@ -108,7 +37,7 @@ const rentalData = [
   {
     type: 'jet-ski', 
     title: 'Yamaha VX Deluxe',
-    imgSource: ['/repository/images/boatRentals/YamahaVXDeluxe.jpg', '/repository/images/boatRentals/YamahaVXDeluxe.jpg', '/repository/images/boatRentals/YamahaVXDeluxe.jpg', '/repository/images/boatRentals/YamahaVXDeluxe.jpg', '/repository/images/boatRentals/YamahaVXDeluxe.jpg', '/repository/images/boatRentals/YamahaVXDeluxe.jpg', '/repository/images/boatRentals/YamahaVXDeluxe.jpg'], 
+    imgSource: ['../images/boatRentals/YamahaVXDeluxe.jpg', '../images/boatRentals/YamahaVXDeluxe.jpg', '../images/boatRentals/YamahaVXDeluxe.jpg', '../images/boatRentals/YamahaVXDeluxe.jpg', '../images/boatRentals/YamahaVXDeluxe.jpg', '../images/boatRentals/YamahaVXDeluxe.jpg', '../images/boatRentals/YamahaVXDeluxe.jpg'], 
     rates: {
       hourly: ['$299', '$339', '$579', '$1,039', '$1,419'], 
       daily: ['$1,299', '$1,069']
@@ -117,7 +46,7 @@ const rentalData = [
   {
     type: 'jet-ski', 
     title: 'Sea-Doo GTI 90',
-    imgSource: ['/repository/images/boatRentals/Sea-DooGTI90.jpeg', '/repository/images/boatRentals/Sea-DooGTI90.jpeg', '/repository/images/boatRentals/Sea-DooGTI90.jpeg', '/repository/images/boatRentals/Sea-DooGTI90.jpeg', '/repository/images/boatRentals/Sea-DooGTI90.jpeg', '/repository/images/boatRentals/Sea-DooGTI90.jpeg', '/repository/images/boatRentals/Sea-DooGTI90.jpeg'], 
+    imgSource: ['../images/boatRentals/Sea-DooGTI90.jpeg', '../images/boatRentals/Sea-DooGTI90.jpeg', '../images/boatRentals/Sea-DooGTI90.jpeg', '../images/boatRentals/Sea-DooGTI90.jpeg', '../images/boatRentals/Sea-DooGTI90.jpeg', '../images/boatRentals/Sea-DooGTI90.jpeg', '../images/boatRentals/Sea-DooGTI90.jpeg'], 
     rates: {
       hourly: ['$299', '$339', '$579', '$1,039', '$1,419'], 
       daily: ['$1,299', '$1,069']
@@ -125,65 +54,7 @@ const rentalData = [
   }
 ]
 
-//CREATES FAQ ITEMS
-const createFAQElement = (faqInfo) => {
-  const element = $('div.faq').append(`
-    <div class="faq-item">
-      <div class="qNa">
-        <h2>Q:&nbsp${faqInfo.question} </h2>
-        <div class="answer hidden"><b>A:&nbsp</b> ${faqInfo.answer}</div>
-      </div>
-      <div class="faq-expand">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
-          <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
-        </svg>
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-dash hidden" viewBox="0 0 16 16">
-          <path d="M4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8z"/>
-        </svg>
-      </div>
-    </div>
-  `)
-  return element;
-}
-
-const generateFAQItems = (faqDataArray) => {
-  for (const faq of faqDataArray) {
-    createFAQElement(faq);
-  }
-}
-
-//FAKE FAQ DATA
-const faqData = [
-  {
-    question: 'IS THERE A DAMAGE DEPOSIT REQUIRED IN ORDER TO RENT A BOAT?',
-    answer: 'Yes. For SeaDoos, Chaparrals, and Cobalts, there is a $500 damage deposit. For Tournament boats, there is a $1000 damage deposit.'
-  }, 
-  {
-    question: 'IS THERE A DAMAGE DEPOSIT REQUIRED IN ORDER TO RENT A BOAT?',
-    answer: 'Yes. For SeaDoos, Chaparrals, and Cobalts, there is a $500 damage deposit. For Tournament boats, there is a $1000 damage deposit.'
-  }, 
-  {
-    question: 'IS THERE A DAMAGE DEPOSIT REQUIRED IN ORDER TO RENT A BOAT?',
-    answer: 'Yes. For SeaDoos, Chaparrals, and Cobalts, there is a $500 damage deposit. For Tournament boats, there is a $1000 damage deposit.'
-  }, 
-  {
-    question: 'IS THERE A DAMAGE DEPOSIT REQUIRED IN ORDER TO RENT A BOAT?',
-    answer: 'Yes. For SeaDoos, Chaparrals, and Cobalts, there is a $500 damage deposit. For Tournament boats, there is a $1000 damage deposit.'
-  }, 
-  {
-    question: 'IS THERE A DAMAGE DEPOSIT REQUIRED IN ORDER TO RENT A BOAT?',
-    answer: 'Yes. For SeaDoos, Chaparrals, and Cobalts, there is a $500 damage deposit. For Tournament boats, there is a $1000 damage deposit.'
-  }, 
-  {
-    question: 'IS THERE A DAMAGE DEPOSIT REQUIRED IN ORDER TO RENT A BOAT?',
-    answer: 'Yes. For SeaDoos, Chaparrals, and Cobalts, there is a $500 damage deposit. For Tournament boats, there is a $1000 damage deposit.'
-  }
-];
-
 $(document).ready(function() {
-  generateRentalItems(rentalData);
-  generateFAQItems(faqData);
-  
   //SHOWS AND HIDES FAQ ANSWERS
   $('.faq-item').on('click', function() {
     const index = $('.faq-item').index(this)
